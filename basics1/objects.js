5d1c94fe270684dbf59aed75c23129e2e7f14357
Marvel0007 <shivam.2024ug1088@iiitranchi.ac.in>
2025-03-27 07:00:44 +0000

    objects

---
+++ basics1/objects.js
@@ -0,0 +1,14 @@
+const user={
+    username: "shivam",
+    class: "skjdhkj",
+    subjects: ["maths","english","science"],
+    address: {
+        city:"prayagraj",
+        state:"UP"
+    }
+}
+console.log(user.username);
+console.log(user.address);
+console.log(user.subjects[1]);
+console.log(user.address.city);
+console.log(user);
